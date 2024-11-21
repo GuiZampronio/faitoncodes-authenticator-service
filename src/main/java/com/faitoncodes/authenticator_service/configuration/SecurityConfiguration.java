9de317864f31734c7e3f56c29ca3fc7d9cb44cc9
Guilherme Zampronio <guizampronio@gmail.com>
2024-11-20 21:14:53 -0300

    Fix cors3

--- src/main/java/com/faitoncodes/authenticator_service/configuration/SecurityConfiguration.java
+++ src/main/java/com/faitoncodes/authenticator_service/configuration/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -32,6 +33,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(
